10396490ef10c629598627c8f5f298682d0dff5a
Ewan <ewan.s@cucumberwifi.io>
2020-03-16 14:02:09 +0000

    says goodbye

--- src/main/java/Ohce.java
+++ src/main/java/Ohce.java
@@ -1,5 +1,9 @@
 public class Ohce {
-    public String reverseWord(String input) {
+    public String respond(String input) {
+        if (input.equals("Stop!")) {
+            return "Adios Pedro";
+        }
+
         String reverse = getReverse(input);
         if (reverse.equals(input)) {
             reverse += "\n¡Bonita palabra!";
--- src/test/java/OhceShould.java
+++ src/test/java/OhceShould.java
@@ -1,17 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OhceShould {
+
+    private Ohce ohce;
+
+    @BeforeEach
+    void setUp() {
+        ohce = new Ohce();
+    }
+
     @Test
     void return_reversal_of_word() {
-        Ohce ohce = new Ohce();
-        assertEquals("ih", ohce.reverseWord("hi"));
+        assertEquals("ih", ohce.respond("hi"));
     }
 
     @Test
     void congratulate_you_when_a_palindrome_is_given() {
-        Ohce ohce = new Ohce();
-        assertEquals("racecar\n¡Bonita palabra!", ohce.reverseWord("racecar"));
+        assertEquals("racecar\n¡Bonita palabra!", ohce.respond("racecar"));
+    }
+
+    @Test
+    void say_goodbye_if_stop_with_exclamation() {
+        assertEquals("Adios Pedro", ohce.respond("Stop!"));
     }
 }
